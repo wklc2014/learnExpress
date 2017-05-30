9a886f956bfe260561af5d5041a51634d9822828
wklc2014 <373658358@qq.com>
2017-05-31 00:43:27 +0800

    route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+var html = {
+    regular: path.resolve(__dirname, '../html/regular.html'),
+    special: path.resolve(__dirname, '../html/special.html')
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', function(req, res, next) {
+
+    if (req.params.id == 0) {
+        next('route');
+    } else {
+        next();
+    }
+
+}, function (req, res, next) {
+    res.sendFile(html.regular);
+});
+
+router.get('/:id', function (req, res, next) {
+    res.sendFile(html.special);
 });
 
 module.exports = router;
